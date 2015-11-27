9194ec5a794f67e9b0adfe36fc3e64f6359fbd3c
Randall Sexton <jrandallsexton@gmail.com>
2015-11-27 12:02:22 -0600

    HttpClient injected into first vm

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+import {inject} from "aurelia-framework";
+import {HttpClient} from "aurelia-http-client";
+
+@inject(HttpClient)
 export class App {
-    constructor() {
+    constructor(httpClient) {
         this.message = "";
+        this.http = httpClient;
     }
     activate() {
         this.message = "Hello from Aurelia!";
+        this.http.get('./movies.json')
+            .then(response => {
+                this.movies = response.content;
+        });
     }
     changeMessage() {
         this.message = "Goodbye :)";
--- config.js
+++ config.js
@@ -5,7 +5,8 @@ System.config({
   babelOptions: {
     "optional": [
       "runtime",
-      "optimisation.modules.system"
+      "optimisation.modules.system",
+      "es7.decorators"
     ]
   },
   paths: {
@@ -16,6 +17,7 @@ System.config({
   map: {
     "aurelia-bootstrapper": "npm:aurelia-bootstrapper@1.0.0-beta.1",
     "aurelia-framework": "npm:aurelia-framework@1.0.0-beta.1.0.2",
+    "aurelia-http-client": "npm:aurelia-http-client@1.0.0-beta.1",
     "babel": "npm:babel-core@5.8.34",
     "babel-runtime": "npm:babel-runtime@5.8.34",
     "core-js": "npm:core-js@1.2.6",
@@ -80,6 +82,11 @@ System.config({
     "npm:aurelia-history-browser@1.0.0-beta.1": {
       "aurelia-history": "npm:aurelia-history@1.0.0-beta.1",
       "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1",
+      "core-js": "npm:core-js@1.2.6"
+    },
+    "npm:aurelia-http-client@1.0.0-beta.1": {
+      "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1",
+      "aurelia-path": "npm:aurelia-path@1.0.0-beta.1",
       "core-js": "npm:core-js@1.2.6"
     },
     "npm:aurelia-loader-default@1.0.0-beta.1.0.1": {
