b42c1083c7ffb191b4e70a09aef0a5fe85afca03
Randall Sexton <jrandallsexton@gmail.com>
2015-11-27 12:40:32 -0600

    introduction of movieData api wrapper.  last commit prior to adding Bootstrap.

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
 import {inject} from "aurelia-framework";
-import {HttpClient} from "aurelia-http-client";
+import {MovieData} from "./movieData";
 
-@inject(HttpClient)
+@inject(MovieData)
 export class App {
-    constructor(httpClient) {
-        this.message = "";
-        this.http = httpClient;
+    constructor(movieData) {
+        this.movieData = movieData;
     }
     activate() {
-        this.message = "Hello from Aurelia!";
-        this.http.get('./movies.json')
-            .then(response => {
-                this.movies = response.content;
-        });
-    }
-    changeMessage() {
-        this.message = "Goodbye :)";
+        return this.movieData.getAll()
+        .then(movies => this.movies = movies);
     }
 }
---
+++ movieData.js
@@ -0,0 +1,20 @@
+/**
+ * Created by RSexton on 11/27/2015.
+ */
+import {inject} from "aurelia-framework";
+import {HttpClient} from "aurelia-http-client";
+
+let baseUrl = './movies.json';
+
+@inject(HttpClient)
+export class MovieData {
+    constructor(httpClient) {
+      this.http = httpClient;
+    };
+    getAll() {
+        return this.http.get(baseUrl)
+            .then(response => {
+                return response.content;
+            });
+    };
+}
